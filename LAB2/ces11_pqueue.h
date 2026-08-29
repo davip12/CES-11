b526740db80b14734ec4be85b86e8a2d768cea01
Davi Pontes <davipontes54@gmail.com>
2026-08-29 15:39:55 -0300

    inicial setup
    criados os arquivos e o Makefile

---
+++ LAB2/ces11_pqueue.h
@@ -0,0 +1,12 @@
+// Arquivo : ces11_pqueue.h
+//
+// [CES -11] T30 .3: Lab01 .II
+// Davi Honorio de Brito Pontes
+// 9389
+
+#ifndef CES11_PQUEUE.H
+#define CES11_PQUEUE.H
+
+
+
+#endif // CES11_PQUEUE.H
